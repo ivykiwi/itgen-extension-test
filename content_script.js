103ddae1fd936be88d45e58949d22009a224a435
nkroe <nikitak123@mail.ru>
2019-12-04 12:01:56 +0500

    fix

--- content_script.js
+++ content_script.js
@@ -35,18 +35,22 @@ const createDate = tz => {
 }
 
 const FILL_C2D_HTML = () => `
-  <div style="width: 260px; height: 40px; margin-top: 8px; display: flex; flex-direction: row; justify-content: space-between;" >
+  <div class="c2d__block" style="width: 260px; height: 40px; margin-top: 8px; display: flex; flex-direction: row; justify-content: space-between;" >
     <span style=" align-self: center; text-align: center; user-select: none;" >Поле c2d не заполнено.</span>
     <button class="btn__sendC2D" style=" align-self: center; width: 100px; height: 25px; font-size: 10.5px; display: flex; flex-direction: row; justify-content: center; text-align: center; border: 1px solid #acacac; background: #fff; cursor: pointer;">
       Заполнить
     </button>
   </div>`;
 
+const C2D_HTML = () => `<div class="c2d__block"></div>`;
+
 const BALANCE_HTML = (balance) => `
-  <span style="margin-top: 4px; font-weight: 600; color: #000; font-size: 12px;">Баланс:</span>
-  <span>
-    ${Object.entries(balance).map(elem => [elem[0] + 'ч', elem[1]].join(': ')).join(', ')}
-  </span>
+  <div class="balance__block">
+    <span style="margin-top: 4px; font-weight: 600; color: #000; font-size: 12px;">Баланс:</span>
+    <span>
+      ${Object.entries(balance).map(elem => [elem[0] + 'ч', elem[1]].join(': ')).join(', ')}
+    </span>
+  </div>
 `;
 
 const loadDialog = (dialogId, type, _lastRequest) => {
@@ -59,13 +63,23 @@ const loadDialog = (dialogId, type, _lastRequest) => {
     return;
 
   } else if (_lastRequest === lastRequest) {
-    let div = document.querySelector('div.left-title-dialog-container div div');
-    if (div) div.innerHTML += ` &nbsp<span>Загрузка...</span>`;
+
+    const div = document.querySelector('div.left-title-dialog-container div div');
+    const nameBlock = document.querySelector('.name__block');
+    const childsParentsBlock = document.querySelector('.childsParents__block');
+    const chatDiv = document.querySelector('div#chat-container > div');
+    const divBalance = document.querySelector('.left-title-dialog-container');
+    const balanceBlock = document.querySelector('.balance__block');
+    const c2dBlock = document.querySelector('.c2d__block');
+
+    if (!document.querySelector('.name__block') && div) div.innerHTML += ` &nbsp<span>Загрузка...</span>`;
     chrome.storage.local.get('token', async ({ token }) => {
+
       if (!token) {
         if (div) div.innerHTML = div.innerHTML.slice(0, -30);
         return;
       }
+
       const responseBody = await fetchResource(`${BASE_URL}/api/v1/c2d/findByDialogId/${dialogId}`, {
         headers: {
           'authorization': `Bearer ${token}`,
@@ -73,62 +87,78 @@ const loadDialog = (dialogId, type, _lastRequest) => {
         }
       })
       const response = await responseBody.json();
+
       if ((response.error === 'USER_NOT_FOUND') && (type === 1)) {
-        if (div) div.innerHTML = div.innerHTML.slice(0, -30);
+
+        if (!document.querySelector('.name__block') && div) div.innerHTML = div.innerHTML.slice(0, -30);
         const phone = document.querySelector('.phone_id') && document.querySelector('.phone_id').innerText.match(/([0-9])/gi) ? document.querySelector('.phone_id').innerText.match(/([0-9])/gi).join('') : null;
         if (!phone) return;
         loadDialog(phone, 2, _lastRequest)
         return;
+
       } else if ((response.error === 'USER_NOT_FOUND') && (type === 2)) {
-        if (div) div.innerHTML = div.innerHTML.slice(0, -30)
+
+        if (!document.querySelector('.name__block') && div) div.innerHTML = div.innerHTML.slice(0, -30)
+        if (!nameBlock && div) {
+          div.innerHTML += `<div class="name__block"></div>`
+          div.style.display = 'flex'
+        };
+        if (!childsParentsBlock && chatDiv) chatDiv.innerHTML = `<div class="childsParents__block"></div>${chatDiv.innerHTML}`;
+        if (!balanceBlock && divBalance && response.balance) divBalance.innerHTML += BALANCE_HTML(response.balance);
+        if (!c2dBlock && divBalance) divBalance.innerHTML += C2D_HTML();
+
         return;
       }
 
-      if (div) div.innerHTML = div.innerHTML.slice(0, -30);
-      if (_lastRequest === lastRequest) {
-        const foundedUser = response.lead || [...response.parents, ...response.childs].find(user => user._id === response.foundedUserId);
+      if (!document.querySelector('.name__block') && div) div.innerHTML = div.innerHTML.slice(0, -30);
 
+      if (_lastRequest === lastRequest) {
+
+        const foundedUser = response.lead || [...response.parents, ...response.childs].find(user => user._id === response.foundedUserId);
         const getUrl = (relativePath) => `${BASE_URL}/${relativePath}`;
         const isParent = () => foundedUser.roles.includes('parent');
         const isLead = () => !foundedUser.roles;
         const isChild = () => !isParent() && !isLead();
         const getRoleName = () => isLead() ? 'лид' : (isParent() ? 'род.' : 'уч.');
-
         const parents = response.parents[0];
 
-        if (div) {
+        if (!nameBlock && div) {
+
           div.innerHTML = div.innerHTML + `
-                  <span style="margin-left: 4px; cursor: ">
-                    (${ getRoleName() })
-                  </span>
-                  <span style="padding-left: 4px; padding-right: 4px; font-weight: 600; color: #000; font-size: 12px; cursor: auto;">
-                    ${foundedUser.tz ? `${createDate(foundedUser.tz)} (${foundedUser.tz})` : ''}
-                  </span>
-                  <span style="padding-left: 2px; padding-right: 2px; font-weight: 600; color: #000; font-size: 12px; cursor: auto;">
-                    ${foundedUser.city ? foundedUser.country + ',' : foundedUser.country} ${foundedUser.city}
-                  </span>&nbsp
-                  ${foundedUser.familyId ? 
-                    `<a href="${getUrl(`family/${foundedUser.familyId}`)}" target="_blank">
-                      <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/greenhome.svg')}" alt="Семья"/>
-                    </a>&nbsp` : 
-                    ''
-                  }
-                  <a href="${ getUrl(`${ isLead() ? 'lead' : 'profile'}/${foundedUser._id }`) } " target="_blank">
-                    <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/profile.svg')}" alt="Профиль" />
-                  </a>
-                  ${ !isChild() ? '' : ( ` &nbsp
-                    <a href="${getUrl(`schedule/${foundedUser._id}`)}" target="_blank">
-                      <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/calendar.svg')}" alt="Расписание"/>
-                    </a> &nbsp 
-                    <a href="${getUrl(`createSchedule/${foundedUser._id}`)}" target="_blank">
-                      <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/plus.svg')}" alt="Записать"/>
-                    </a>`
-                    )
-                  }`
+                  <div class="name__block">
+                    <span style="margin-left: 4px; cursor: ">
+                      (${ getRoleName() })
+                    </span>
+                    <span style="padding-left: 4px; padding-right: 4px; font-weight: 600; color: #000; font-size: 12px; cursor: auto;">
+                      ${foundedUser.tz ? `${createDate(foundedUser.tz)} (${foundedUser.tz})` : ''}
+                    </span>
+                    <span style="padding-left: 2px; padding-right: 2px; font-weight: 600; color: #000; font-size: 12px; cursor: auto;">
+                      ${foundedUser.city ? foundedUser.country + ',' : foundedUser.country} ${foundedUser.city}
+                    </span>&nbsp
+                    ${foundedUser.familyId ? 
+                      `<a href="${getUrl(`family/${foundedUser.familyId}`)}" target="_blank">
+                        <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/greenhome.svg')}" alt="Семья"/>
+                      </a>&nbsp` : 
+                      ''
+                    }
+                    <a href="${ getUrl(`${ isLead() ? 'lead' : 'profile'}/${foundedUser._id }`) } " target="_blank">
+                      <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/profile.svg')}" alt="Профиль" />
+                    </a>
+                    ${ !isChild() ? '' : ( ` &nbsp
+                      <a href="${getUrl(`schedule/${foundedUser._id}`)}" target="_blank">
+                        <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/calendar.svg')}" alt="Расписание"/>
+                      </a> &nbsp 
+                      <a href="${getUrl(`createSchedule/${foundedUser._id}`)}" target="_blank">
+                        <img style="width: 15px; height: 15px" src="${chrome.extension.getURL('assets/plus.svg')}" alt="Записать"/>
+                      </a>`
+                      )
+                    }
+                  </div>`
 
           div.style.color = '#c51c1c';
+          div.style.display = 'flex';
 
-          Array.from(div.children).forEach((elem, index) => {
+          Array.from(document.querySelector('.name__block').children).forEach((elem, index) => {
             if (index !== 0) {
               elem.addEventListener('click', e => {
                 e.stopPropagation()
@@ -136,14 +166,14 @@ const loadDialog = (dialogId, type, _lastRequest) => {
               })
             }
           })
+
         };
 
-        const chatDiv = document.querySelector('div#chat-container > div');
         const familyUsers = isLead() ? [] : isParent() ? response.childs : response.parents;
 
-        if (chatDiv && familyUsers.length) {
+        if (!childsParentsBlock && chatDiv && familyUsers.length) {
           chatDiv.innerHTML = `
-            <div style="position: absolute; width: auto; height: auto; border: 1px solid rgba(0,0,0,0.2); padding: 2px 10px; background: #fff; z-index: 10" >
+            <div class="childsParents__block" style="position: absolute; width: auto; height: auto; border: 1px solid rgba(0,0,0,0.2); padding: 2px 10px; background: #fff; z-index: 10" >
             ${ familyUsers.map(elem => `
                       <div style="padding: 2px 0">
                         <a href="${getUrl(`profile/${elem._id}`)}"> ${elem.firstName} ${elem.lastName} </a> &nbsp 
@@ -160,14 +190,14 @@ const loadDialog = (dialogId, type, _lastRequest) => {
             </div>${chatDiv.innerHTML}`;
         }
 
-        const divBalance = document.querySelector('.left-title-dialog-container');
-
-        if (divBalance && response.balance) {
+        if (!balanceBlock && divBalance && response.balance) {
           divBalance.innerHTML += BALANCE_HTML(response.balance);
         }
 
-        if (divBalance && type === 2) {
+        if (!c2dBlock && divBalance && type === 2) {
           divBalance.innerHTML += FILL_C2D_HTML();
+        } else {
+          divBalance.innerHTML += C2D_HTML();
         }
 
         const btnC2D = document.querySelector('.btn__sendC2D');
